956dbf89b7004acc13277d0da7b97203ab7d604f
filvecchiato <fil.vecchiato@gmail.com>
2020-10-02 19:33:48 +0200

    fix: updated tests

--- screens/Wrapper.js
+++ screens/Wrapper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Explore from './Explore';
-import Curate from './Curate';
+// import Curate from './Curate';
 import Upload from './Upload';
 import UserGallery from './UserGallery';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -21,7 +21,7 @@ const DrawerNavigator = () => {
     >
       <Drawer.Screen name="User Photos" component={UserGallery} />
       <Drawer.Screen name="Explore" component={Explore} />
-      <Drawer.Screen name="Curate" component={Curate} />
+      {/* <Drawer.Screen name="Curate" component={Curate} /> */}
       <Drawer.Screen name="Upload" component={Upload} />
     </Drawer.Navigator>
   );
