88fd372b5f9ab183297162b530c2194d04efba8c
filvecchiato <fil.vecchiato@gmail.com>
2020-10-03 12:14:53 +0200

    refactor: refractored inputs to call single input container

--- components/tests/AppButton.test.js
+++ components/tests/AppButton.test.js
@@ -3,6 +3,17 @@ import renderer from 'react-test-renderer';
 import AppButton from '../AppButton';
 
 test('renders correctly', () => {
-  const tree = renderer.create(<AppButton />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const tree = renderer.create(<AppButton title="test" />);
+  expect(tree.toJSON()).toMatchSnapshot();
+});
+
+test('Pass the correct title from props', () => {
+  const tree = renderer.create(<AppButton title="test" />);
+  const instance = tree.root;
+  expect(instance.props.title).toBe('test');
+});
+
+test('The text component contain the prop value', () => {
+  const tree = renderer.create(<AppButton title="test" />);
+  expect(tree.toJSON().children[0].children[0]).toBe('test');
 });
